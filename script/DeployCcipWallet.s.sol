c7365dc90ff133bea63e72b79de31411712e42be
Aayush Gupta <guptaaayush432@gmail.com>
2023-10-12 21:45:52 +0530

    Created DeployCcipWallet.s.sol

---
+++ script/DeployCcipWallet.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import {Helper} from "./Helper.s.sol";
+import {CcipWallet} from "../src/CcipWallet.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+
+contract DeployBasicTokenSender is Script, Helper {
+    function run(SupportedNetworks source) external {
+        uint256 senderPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(senderPrivateKey);
+
+        (address router, address linkToken, , ) = getConfigFromNetwork(source);
+
+        CcipWallet basicTokenSender = new CcipWallet(router, linkToken);
+
+        console.log(
+            "Basic Token Sender deployed on ",
+            networks[source],
+            "with address: ",
+            address(basicTokenSender)
+        );
+
+        vm.stopBroadcast();
+    }
+}
